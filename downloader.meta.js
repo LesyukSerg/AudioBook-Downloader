b695d50485d9a2455014e0ab3a001bdd750157da
Sergiy <seranatles@gmail.com>
2017-10-03 15:28:05 +0300

    Create downloader.meta.js

---
+++ downloader.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         AudioBook Downloader
+// @version      0.1
+// @author       Lesyuk Serg
+// @description  try to take over the world!
+// @author       Lesyuk Serg
+// @match        https://audioknigi.club/*
+//include        https://audioknigi.club/*
+// @updateURL    https://raw.githubusercontent.com/LesyukSerg/AudioBook-Downloade/master/downloader.meta.js
+// @downloadURL  https://raw.githubusercontent.com/LesyukSerg/AudioBook-Downloade/master/downloader.user.js
+// @grant        none
+// ==/UserScript==
